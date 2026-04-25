24d6db3a48dffc1247e8e9eb8e5b5dd2e073e899
FongFox <phong.tgn.coder@gmail.com>
2026-04-25 18:05:23 +0700

    feat: 📖 add Swagger UI with springdoc-openapi (ep04)

--- vetautet-controller/src/main/java/com/vetautet/ddd/controller/resource/TicketDetailController.java
+++ vetautet-controller/src/main/java/com/vetautet/ddd/controller/resource/TicketDetailController.java
@@ -4,6 +4,8 @@ import com.vetautet.ddd.application.service.ticket.ITicketDetailAppService;
 import com.vetautet.ddd.controller.model.enums.ResultUtil;
 import com.vetautet.ddd.controller.model.vo.ResultMessage;
 import com.vetautet.ddd.domain.model.entity.TicketDetail;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/ticket")
 @Slf4j
+@Tag(name = "Ticket", description = "Ticket Detail APIs")
 public class TicketDetailController {
 
     @Autowired
     private ITicketDetailAppService ticketDetailAppService;
 
     @GetMapping("/{ticketId}/detail/{detailId}")
+    @Operation(summary = "Get ticket detail", description = "Get ticket detail by ticketId and detailId")
     public ResultMessage<TicketDetail> getTicketDetail(
             @PathVariable("ticketId") Long ticketId,
             @PathVariable("detailId") Long detailId
